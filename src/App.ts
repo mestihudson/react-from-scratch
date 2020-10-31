960e6d7f4d6254cfa35cd1fd80a11ef669ef57c6
mestihudson <mestihudson@gmail.com>
2020-10-31 14:15:54 -0300

    refactor: react app basic skeleton

--- src/App.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export interface AppProps {}
-
-export default function App(props: AppProps) {
-  return (
-    <h1>React (with Typescript) From Scratch</h1>
-  )
-}
---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/index.css';
+
+function App() {
+  return (
+    <h1>React (with Typescript) From Scratch</h1>
+  );
+}
+
+export default App;
